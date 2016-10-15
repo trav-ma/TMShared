8baf26802337e5e1078c46a18548c63582df13bb
dstarsboy <tl1000s1998@yahoo.com>
2016-10-15 08:41:44 -0700

    Updated circle graph to be more dynamic

--- TMShared/CircleGraphView.swift
+++ TMShared/CircleGraphView.swift
@@ -10,55 +10,54 @@ import UIKit
 
 class CircleGraphView: UIView {
     var circles = [CAShapeLayer]()
-    
     var percent1: CGFloat = 0
     var percent2: CGFloat = 0
 
     func degreesToRadians(_ degrees: CGFloat) -> CGFloat {
         return degrees * CGFloat(M_PI) / 180.0
     }
-    
-    override func draw(_ rect: CGRect) {
-        let p1 = 360.0 * (CGFloat(percent1) * 0.01)
-        let p2 = 360.0 * (CGFloat(percent2) * 0.01)
-        
+
+    func drawCircleGraph(percents: [CGFloat], colors: [UIColor], thickness: CGFloat = 3) {
+        for circle in circles {
+            circle.removeFromSuperlayer()
+        }
+        circles = []
         let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
         let radius: CGFloat = max(bounds.width, bounds.height)
-        let arcWidth: CGFloat = 3
-        
-        let circle1 = CAShapeLayer()
-        circle1.path = UIBezierPath(arcCenter: center, radius: radius/2 - arcWidth/2, startAngle: degreesToRadians(0), endAngle: degreesToRadians(p1), clockwise: true).cgPath
-        circle1.fillColor = UIColor.clear.cgColor
-        circle1.strokeColor = colorTint.cgColor
-        circle1.lineWidth = arcWidth
-        self.layer.addSublayer(circle1)
+        var total: CGFloat = 0
+        var degrees = [CGFloat]()
+        for percent in percents {
+            total += percent
+            degrees.append(360.0 * (CGFloat(percent) * 0.01))
+        }
+        if total > 100 {
+            return
+        }
+        if total < 100 {
+            degrees.append(360.0 * (CGFloat(100 - total) * 0.01))
+        }
         let drawAnimation = CABasicAnimation(keyPath: "strokeEnd")
         drawAnimation.fromValue = 0.0
         drawAnimation.toValue = 1.0
         drawAnimation.duration = 1.0
         drawAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        circle1.add(drawAnimation, forKey: "drawCircleAnimation")
-        
-        let circle2 = CAShapeLayer()
-        circle2.path = UIBezierPath(arcCenter: center, radius: radius/2 - arcWidth/2, startAngle: degreesToRadians(p1), endAngle: degreesToRadians(p1 + p2), clockwise: true).cgPath
-        circle2.fillColor = UIColor.clear.cgColor
-        circle2.strokeColor = colorOrange.cgColor
-        circle2.lineWidth = arcWidth
-        self.layer.addSublayer(circle2)
-        circle2.add(drawAnimation, forKey: "drawCircleAnimation")
-        
-        let circle3 = CAShapeLayer()
-        circle3.path = UIBezierPath(arcCenter: center, radius: radius/2 - arcWidth/2, startAngle: degreesToRadians(p1 + p2), endAngle: degreesToRadians(360), clockwise: true).cgPath
-        circle3.fillColor = UIColor.clear.cgColor
-        circle3.strokeColor = colorRed.cgColor
-        circle3.lineWidth = arcWidth
-        self.layer.addSublayer(circle3)
-        circle3.add(drawAnimation, forKey: "drawCircleAnimation")
+        var startDegree: CGFloat = 0
+        for index in 0 ..< degrees.count {
+            let degree = degrees[index]
+            let circle = CAShapeLayer()
+            circle.path = UIBezierPath(arcCenter: center, radius: radius/2 - thickness/2, startAngle: degreesToRadians(startDegree), endAngle: degreesToRadians(startDegree + degree), clockwise: true).cgPath
+            startDegree += degree
+            circle.fillColor = UIColor.clear.cgColor
+            if index < colors.count {
+                circle.strokeColor = colors[index].cgColor
+            } else {
+                circle.strokeColor = UIColor.lightGray.cgColor
+            }
+            circle.lineWidth = thickness
+            self.layer.addSublayer(circle)
+            circle.add(drawAnimation, forKey: "drawCircleAnimation")
+            circles.append(circle)
+        }
     }
 
 }
-
-//usage
-//circleGraphView.percent1 = goodPercent
-//circleGraphView.percent2 = badPercent
-//circleGraphView.setNeedsDisplay()
