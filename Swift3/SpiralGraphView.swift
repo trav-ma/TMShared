9a194d7c28da17970f00edd967c0e0b872186740
Travis Ma <travis.ma@Travis-IMS-MacBook-Pro.local>
2017-08-30 10:40:58 -0700

    Added spiral graph

--- Swift3/SpiralGraphView.swift
+++ Swift3/SpiralGraphView.swift
@@ -1,58 +1,51 @@
 //
-//  CircleGraphView.swift
-//  ThreeBite
+//  SpiralCircleGraphView.swift
+//  OCE
 //
-//  Created by Travis Ma on 7/26/15.
-//  Copyright (c) 2015 Travis Ma. All rights reserved.
+//  Created by Travis Ma on 8/30/17.
+//  Copyright © 2017 QIMS. All rights reserved.
 //
 
 import UIKit
 
-class CircleGraphView: UIView {
+class SpiralGraphView: UIView {
     var circles = [CAShapeLayer]()
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = UIColor.clear
     }
-
+    
     func degreesToRadians(_ degrees: CGFloat) -> CGFloat {
         return degrees * CGFloat(Double.pi) / 180.0
     }
     
     //Example percents value [23.4, 35.7, 10.2]
-    func drawCircleGraph(percents: [CGFloat], colors: [UIColor], thickness: CGFloat = 3) {
+    func drawSpiralGraph(percents: [CGFloat], colors: [UIColor], thickness: CGFloat = 10) {
         for circle in circles {
             circle.removeFromSuperlayer()
         }
         circles = []
         let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
-        let radius: CGFloat = max(bounds.width, bounds.height)
-        var total: CGFloat = 0
         var degrees = [CGFloat]()
         for percent in percents {
-            total += percent
             degrees.append(360.0 * (CGFloat(percent) * 0.01))
-        }
-        if total > 100 {
-            print("CircleGraphView Error: Percent adds up to more than 100!")
-            return
-        }
-        if total < 100 {
-            degrees.append(360.0 * (CGFloat(100 - total) * 0.01))
         }
         let drawAnimation = CABasicAnimation(keyPath: "strokeEnd")
         drawAnimation.fromValue = 0.0
         drawAnimation.toValue = 1.0
         drawAnimation.duration = 1.0
-        drawAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        var startDegree: CGFloat = 0
+        drawAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        var radius: CGFloat = max(bounds.width, bounds.height)
         for index in 0 ..< degrees.count {
             let degree = degrees[index]
             let circle = CAShapeLayer()
-            circle.path = UIBezierPath(arcCenter: center, radius: radius/2 - thickness/2, startAngle: degreesToRadians(startDegree), endAngle: degreesToRadians(startDegree + degree), clockwise: true).cgPath
-            startDegree += degree
+            circle.path = UIBezierPath(arcCenter: center, radius: radius/2 - thickness/2,
+                                       startAngle: degreesToRadians(-90),
+                                       endAngle: degreesToRadians(degree - 90),
+                                       clockwise: true).cgPath
             circle.fillColor = UIColor.clear.cgColor
+            circle.lineCap = kCALineCapRound
             if index < colors.count {
                 circle.strokeColor = colors[index].cgColor
             } else {
@@ -62,6 +55,7 @@ class CircleGraphView: UIView {
             self.layer.addSublayer(circle)
             circle.add(drawAnimation, forKey: "drawCircleAnimation")
             circles.append(circle)
+            radius -= (thickness * 2)
         }
     }
 
