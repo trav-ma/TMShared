dddd151a357cd991d0b5156dd73bdbd194daffa9
dstarsboy <tl1000s1998@yahoo.com>
2017-10-22 12:21:24 -0700

    Added modal text entry

---
+++ ModalTextEntryViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ModalTextEntryViewController.swift
+//  RidePaper
+//
+//  Created by Travis Ma on 10/22/17.
+//  Copyright © 2017 Travis Ma. All rights reserved.
+//
+
+import UIKit
+
+protocol ModalTextEntryViewControllerDelegate {
+    func modalTextEntryViewDidChangeText(text: String)
+}
+
+class ModalTextEntryViewController: UIViewController {
+    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var btnDismiss: UIButton!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var textViewNote: UITextView!
+    var borderColor: UIColor?
+    var titleText: String?
+    var titleFont: UIFont?
+    var iconDismiss: UIImage?
+    var iconSave: UIImage?
+    var initialText : String?
+    var delegate: ModalTextEntryViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let borderColor = borderColor {
+            textViewNote.layer.borderColor = borderColor.cgColor
+            textViewNote.layer.borderWidth = 2
+            textViewNote.layer.cornerRadius = 12
+            btnSave.tintColor = borderColor
+            btnDismiss.tintColor = borderColor
+        }
+        labelTitle.text = titleText
+        textViewNote.text = initialText
+        if let titleFont = titleFont {
+            labelTitle.font = titleFont
+        }
+        if let iconSave = iconSave {
+            btnSave.setImage(iconSave, for: .normal)
+            btnSave.setTitle("", for: .normal)
+        }
+        if let iconDismiss = iconDismiss {
+            btnDismiss.setImage(iconDismiss, for: .normal)
+            btnDismiss.setTitle("", for: .normal)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textViewNote.becomeFirstResponder()
+    }
+    
+    @IBAction func btnSaveTap(_ sender: UIButton) {
+        textViewNote.resignFirstResponder()
+        self.dismiss(animated: true, completion: {
+            self.delegate?.modalTextEntryViewDidChangeText(text: self.textViewNote.text ?? "")
+        })
+    }
+    
+    @IBAction func btnDismissTap(_ sender: UIButton) {
+        textViewNote.resignFirstResponder()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
