6c9794b5451a5be414662b2b2dbb5d51565c4d38
Travis Ma <tl1000s1998@yahoo.com>
2019-07-09 00:47:33 -0700

    added date+extensions for endOfDay etc

--- Date+Extensions.swift
+++ Date+Extensions.swift
@@ -65,4 +65,26 @@ extension Date {
         return cal.component(.weekday, from: self)
     }
     
+    var startOfDay: Date {
+        return Calendar.current.startOfDay(for: self)
+    }
+    
+    var endOfDay: Date {
+        var components = DateComponents()
+        components.day = 1
+        components.second = -1
+        return Calendar.current.date(byAdding: components, to: startOfDay)!
+    }
+    
+    var startOfMonth: Date {
+        let components = Calendar.current.dateComponents([.year, .month], from: startOfDay)
+        return Calendar.current.date(from: components)!
+    }
+    
+    var endOfMonth: Date {
+        var components = DateComponents()
+        components.month = 1
+        components.second = -1
+        return Calendar.current.date(byAdding: components, to: startOfMonth)!
+    }
 }
