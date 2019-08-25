a4986e1602b033c6a0e3889b70b5c20cc9eda9b8
dstarsboy <tl1000s1998@yahoo.com>
2019-08-24 18:12:25 -0700

    Added CoreDataHelper

---
+++ CoreDataHelper.swift
@@ -0,0 +1,71 @@
+//
+//  CoreDataHelper.swift
+//  DailyFast
+//
+//  Created by Travis Ma on 8/24/19.
+//  Copyright © 2019 Travis Ma. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+let databaseContainer: NSPersistentContainer = {
+    print(NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory, .userDomainMask, true).last! as String)
+    let container = NSPersistentContainer(name: "Model")
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        if let error = error {
+            fatalError("Unresolved error \(error)")
+        }
+    })
+    return container
+}()
+
+var dbContext: NSManagedObjectContext {
+    return databaseContainer.viewContext
+}
+
+//example upsert
+//dbContext.performAndWait {
+//    let request: NSFetchRequest<Day> = Day.fetchRequest()
+//    request.predicate = NSPredicate(format: "dayId = %@", dayId)
+//    request.fetchLimit = 1
+//    let dayRecord: Day!
+//    if let days = try? request.execute(), let day = days.first {
+//        dayRecord = day
+//    } else {
+//        dayRecord = Day(context: dbContext)
+//    }
+//    dayRecord.dayId = dayId
+//    dayRecord.isDayOff = false
+//    try? dbContext.save()
+//}
+
+//example fetch
+//dbContext.performAndWait {
+//    let request: NSFetchRequest<Day> = Day.fetchRequest()
+//    request.predicate = NSPredicate(format: "dayId = %@", dayId)
+//    request.fetchLimit = 1
+//    if let dayRecs = try? request.execute(), let dayRec = dayRecs.first, !dayRec.isDayOff {
+//        prevDate = dayRec.endFeeding
+//    }
+//}
+
+extension NSManagedObjectContext {
+    
+    /// Executes the given `NSBatchDeleteRequest` and directly merges the changes to bring the given managed object context up to date.
+    ///
+    /// - Parameter batchDeleteRequest: The `NSBatchDeleteRequest` to execute.
+    /// - Throws: An error if anything went wrong executing the batch deletion.
+    
+//    let fetchRequest: NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Day")
+//    let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+//    try? dbContext.executeAndMergeChanges(using: batchDeleteRequest)
+    
+    public func executeAndMergeChanges(using batchDeleteRequest: NSBatchDeleteRequest) throws {
+        batchDeleteRequest.resultType = .resultTypeObjectIDs
+        let result = try execute(batchDeleteRequest) as? NSBatchDeleteResult
+        let changes: [AnyHashable: Any] = [NSDeletedObjectsKey: result?.result as? [NSManagedObjectID] ?? []]
+        NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [self])
+    }
+}
+
